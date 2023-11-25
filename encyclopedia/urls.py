b731142cde546b587202ab5666bc81f37bef0e31
glovastefan <glovastefan@gmail.com>
2023-11-25 19:20:44 +0000

    third task done 'Search: Allow the user to type a query into the search box in the sidebar to search for an encyclopedia entry.'

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<title>", views.entry_by_title, name="entry_by_title"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,12 +4,32 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+
 
 def entry_by_title(request, title):
-    return render(request, "encyclopedia/view_entry.html", {
-        "entry": util.get_entry(title=title),
-        "title": title
-    })
+    return render(
+        request,
+        "encyclopedia/view_entry.html",
+        {"entry": util.get_entry(title=title), "title": title},
+    )
+
+
+def search(request):
+    if request.method == "POST":
+        search_entry = request.POST.get("q", None)
+        all_entries = util.list_entries()
+        if search_entry in all_entries:
+            return entry_by_title(request, title=search_entry)
+        else:
+            to_display = [
+                entry for entry in all_entries if search_entry.lower() in entry.lower()
+            ]
+            if to_display:
+                return render(
+                    request, "encyclopedia/index.html", {"entries": to_display}
+                )
+            else:
+                return entry_by_title(request, title=search_entry)
+    else:
+        return render(request, "encyclopedia/index.html")
