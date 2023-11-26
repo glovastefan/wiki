c0a9d451c625dd4035f3f1d47cefc92630d3e5d7
glovastefan <glovastefan@gmail.com>
2023-11-26 01:19:47 +0000

    bug fixed at views.py where entry_by_title when method was POST was getting mixed if it comes from search and edit, now the edit behavior was only expected if comes from edit which was fixed by getting source of POST method

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,16 +8,20 @@ def index(request):
 
 
 def entry_by_title(request, title):
+    entry = markdown2.markdown(util.get_entry(title=title))
     if request.method == "POST":
-        return render(request, "encyclopedia/edit.html", {
-            "title": title
-        })
+        source = request.POST.get("source", "")
+        if source == "search":
+            return render(request, "encyclopedia/edit.html", {"title": title})
+        else:
+            return render(
+                request,
+                "encyclopedia/view_entry.html",
+                {"entry": entry, "title": title},
+            )
     else:
-        entry = markdown2.markdown(util.get_entry(title=title))
         return render(
-            request,
-            "encyclopedia/view_entry.html",
-            {"entry": entry, "title": title}
+            request, "encyclopedia/view_entry.html", {"entry": entry, "title": title}
         )
 
 
@@ -25,7 +29,7 @@ def search(request):
     if request.method == "POST":
         search_entry = request.POST.get("q", None)
         all_entries = util.list_entries()
-        if search_entry in [title.lower() for title in util.list_entries()]:
+        if search_entry.lower() in [title.lower() for title in util.list_entries()]:
             return entry_by_title(request, title=search_entry)
         else:
             to_display = [
@@ -33,27 +37,34 @@ def search(request):
             ]
             if to_display:
                 return render(
-                    request, "encyclopedia/index.html", {"entries": to_display, "search": True}
+                    request,
+                    "encyclopedia/index.html",
+                    {"entries": to_display, "search": True},
                 )
             else:
                 return entry_by_title(request, title=search_entry)
     else:
         return render(request, "encyclopedia/index.html")
-    
+
+
 def new_entry(request):
     if request.method == "POST":
         new_title = request.POST.get("new_title")
         new_page = request.POST.get("new_entry_text")
         if not new_title:
-            return render(request, "encyclopedia/error.html", {
-                "error_message": "Title must be provided"
-            })
+            return render(
+                request,
+                "encyclopedia/error.html",
+                {"error_message": "Title must be provided"},
+            )
         else:
             all_entries = [title.lower() for title in util.list_entries()]
             if new_title.lower() in all_entries:
-                return render(request, "encyclopedia/error.html", {
-                    "error_message": f"Title {new_title} already exist!"
-                })
+                return render(
+                    request,
+                    "encyclopedia/error.html",
+                    {"error_message": f"Title {new_title} already exist!"},
+                )
             else:
                 with open(f"entries/{new_title}.md", "w") as new_file:
                     print(f"# {new_title}\n", file=new_file)
@@ -63,6 +74,7 @@ def new_entry(request):
     else:
         return render(request, "encyclopedia/new_page.html")
 
+
 def edit(request, title):
     if request.method == "POST":
         edited_entry = request.POST.get("new_entry_text")
@@ -70,17 +82,22 @@ def edit(request, title):
         return render(
             request,
             "encyclopedia/view_entry.html",
-            {"entry": util.get_entry(title=title), "title": title}
+            {"entry": util.get_entry(title=title), "title": title},
         )
     else:
-        return render(request, "encyclopedia/edit.html",
-            {"entry": util.get_entry(title=title), "title": title})
+        return render(
+            request,
+            "encyclopedia/edit.html",
+            {"entry": util.get_entry(title=title), "title": title},
+        )
+
 
 def random(request):
     import random
+
     random_entry = random.choice(util.list_entries())
     return render(
-            request,
-            "encyclopedia/view_entry.html",
-            {"entry": util.get_entry(title=random_entry), "title": random_entry},
-        )
+        request,
+        "encyclopedia/view_entry.html",
+        {"entry": util.get_entry(title=random_entry), "title": random_entry},
+    )
